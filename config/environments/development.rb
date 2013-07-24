b9d1e452f6b7ced0307cc4ad261d4e28a15d487b
Stephen Nagy <stnagy@gmail.com>
2013-07-24 15:20:49 -0400

    First attempt at mailer app edit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,5 +40,5 @@ Omrails::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
 
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'http://calm-journey-6114.herokuapp.com/' }
 end
